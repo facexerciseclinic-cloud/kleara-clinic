ba86ab5247fe8c21a5f1a9cef12ab32407d9e58b
Kleara <facexerciseclinic@gmail.com>
2025-10-16 17:28:22 +0700

    Fix POS page api import - use named export

--- client/src/pages/POS.tsx
+++ client/src/pages/POS.tsx
@@ -1,6 +1,6 @@
 // React Core
 import React, { useState, useEffect } from 'react';
-import api from '../services/api';
+import { api } from '../services/api';
 
 // Components
 import GridItem from '../components/common/GridItem';
